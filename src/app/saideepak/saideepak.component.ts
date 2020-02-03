f7a53527ebdb7ea3e7ab08af7794a6674d58243a
SaiDeepak.B <SaiDeepak.B@ibo.org>
2020-02-03 20:42:40 +0530

    Changes for Yearly Calendar

--- src/app/saideepak/saideepak.component.ts
+++ src/app/saideepak/saideepak.component.ts
@@ -32,6 +32,7 @@ export class SaideepakComponent implements OnInit {
     this.passMonthYear(new Date(this.getYear).getFullYear());
     this.weeklyCalendarLoad(this.getWeek);
     this.getFullMonth(this.getMonth);
+    console.log(this.appendWeekDates(new Date("02/01/2020"),new Date("02/29/2020")));
     // console.log(this.checkActive("01/27/2020","02/04/2020","01/28/2020"));
     this.weeklyEvents = ([
       {
@@ -343,7 +344,8 @@ export class SaideepakComponent implements OnInit {
           monthFullName: this.getMonthName(i)['fullName'],
           monthHalfName: this.getMonthName(i)['halfName'],
           monthLetter: this.getMonthName(i)['letter'],
-          weeks: this.getWeeksStartAndEndInMonth(year, i)
+          weeks: this.getWeeksYear(year, i),
+          calendarWeeks: this.getWeeksStartAndEndInMonth(year, i)
         }
       );
     }
@@ -351,6 +353,29 @@ export class SaideepakComponent implements OnInit {
     console.log(this.currentYearAllMonths);
   }
 
+  loopWeeks(weekStart,weekEnd){
+    var weekLoad = [];
+    // for()
+  }
+  getWeeksYear(year: number, month: number){
+    month = month+1;
+    var getTotalWeeks = new Date(year, month, 0).getDate();
+    var totalArray = [];
+    var totalWeeks = Math.ceil(getTotalWeeks/7);
+    var lastDay = 0;
+    for(let i=1;i<=totalWeeks;i++){
+      var newLastDay = lastDay+7;
+      if(i==totalWeeks){
+        newLastDay = getTotalWeeks;
+      }
+      totalArray.push({
+        'start': lastDay+1,
+        'end': newLastDay
+      })
+      lastDay = i*7;
+    }
+    return totalArray;
+  }
   getWeeksStartAndEndInMonth(_year: number, _month: number, returnDate: boolean = false) {
     const lastDay = new Date(_year, _month + 1, 0);
     let weeks: Array<{start: number, end: number}> = [];
